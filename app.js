883a96fbfcc50f5c5b26234e88f969e81df484ba
jkumari25 <jagriti.sj15@gmail.com>
2023-07-25 11:18:52 +0530

    updated

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express= require('express');
 const errorMiddleware= require("./middleware/error");
+const cors= require('cors');
 
 const app= express();
 app.use(express.json());
+app.use(cors({
+    origin:"*"
+}));
 
 app.get("/",(req,res)=>{
     res.send("HOME PAGE")
