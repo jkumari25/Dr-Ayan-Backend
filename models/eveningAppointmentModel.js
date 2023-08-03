c788d1d768a042530b21a48660df2324ffddc8f4
jkumari25 <jagriti.sj15@gmail.com>
2023-08-03 17:56:50 +0530

    added

--- models/eveningAppointmentModel.js
+++ models/eveningAppointmentModel.js
@@ -4,7 +4,7 @@ const eveningAppointmentSchema= new mongoose.Schema({
     showEndDate: {type: String},
     isEvening:{type:String},
     isEveningTimeShow:{type:String},
-    status:{type:Boolean}
+    status:{type:String}
 })
 
 module.exports = mongoose.model("EveningAppointment",eveningAppointmentSchema);
